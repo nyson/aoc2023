bc84f88d4a6a478136eeaedb33f609f2390de1ba
Jonathan Skårstedt <jonathan@hazeltime.com>
2023-12-14 18:55:27 +0100

    uglified day12

--- aoc2023/day12.py
+++ aoc2023/day12.py
@@ -17,59 +17,43 @@ def parse_pt2(f: TextIOWrapper) -> Iterator[tuple[str, list[int]]]:
 def kf(s: str, rs: list[int], con: bool) -> int:
     return hash((s, ':'.join(map(str, rs)), con))
 
-memo: dict[int,int] = {}
-def all_sols(p: str, rs: list[int], consuming: bool = False, prefix:str="") -> int:
-    if len(p)==0:
-        if sum(rs) == 0:
-            return 1
-        else:
-            return 0
 
-    match memo.get(kf(p, rs, consuming)):
+memo: dict[int,int] = {}
+def memoize(key: int, val: int) -> int:
+    memo[key] = val
+    return val
+
+def all_sols(p: str, rs: list[int], consuming: bool = False) -> int:
+    mkey = kf(p, rs, consuming)
+
+    if len(p)==0:
+        return 1 if sum(rs) == 0 else 0
+
+    match memo.get(mkey):
         case None: pass
         case i: return i
 
     match p[0], p[1:]:
         # broken but we got runs left
         case '#', rest if len(rs) > 0 and rs[0] > 0:
-            rsN = [rs[0] - 1, *rs[1:]]
-            sol = all_sols(rest, rsN, consuming=True, prefix=prefix + '#')
-            memo[kf(p, rs, consuming)] = sol
-            return sol
-
+            return memoize(mkey, all_sols(rest, [rs[0] - 1, *rs[1:]], consuming=True))
         # no possible solutions unless we can consume runs
         case '#', _:
             return 0
-
         # ok but we have an empty rs we need to pop
         case '.', rest if consuming and len(rs) > 0 and rs[0] == 0:
-            sol = all_sols(rest, rs[1:], consuming=False, prefix=prefix + '.')
-            memo[kf(p, rs, consuming)] = sol
-            return sol
-
+            return memoize(mkey, all_sols(rest, rs[1:], consuming=False))
         # no possible oks as a consuming state wih none left to consume is bad
         case '.', rest if consuming:
             return 0
-
         # ok as we are not consuming
         case '.', rest:
-            sol = all_sols(rest, rs, consuming, prefix=prefix + '.')
-            memo[kf(p, rs, consuming)] = sol
-            return sol
-
+            return memoize(mkey, all_sols(rest, rs, consuming))
         # branching case
         case '?', rest:
-            ok_sols = all_sols('.' + rest, rs, consuming, prefix)
-            broken_sols = all_sols('#' + rest, rs, consuming, prefix)
-            return ok_sols + broken_sols
-
+            return all_sols('.' + rest, rs, consuming) + all_sols('#' + rest, rs, consuming)
         case invalid:
-            conditions = [
-                ("len(rs) > 0 and rs[0] > 0", len(rs) > 0 and rs[0] > 0),
-                ("if len(rs) > 0 and rs[0] == 0", len(rs) > 0 and rs[0] == 0)]
-
-            nl = "\n\t"
-            raise ValueError(f"{invalid}: \n\t" + nl.join([f"{exp} => {evl}" for exp, evl in conditions]))
+            raise ValueError(invalid)
 
 def part1(f: TextIOWrapper):
     s = 0
@@ -80,7 +64,6 @@ def part1(f: TextIOWrapper):
         print(f" => {sols} solutions!")
 
     print(f"{s} total solutions!")
-
 
 def run(f: TextIOWrapper, part: int):
     if part == 2:
